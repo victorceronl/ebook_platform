6551af642bf3fc595ae218114571e1d0a8cce4a8
@victorceronl <ceronloezavictor@gmail.com>
2025-07-31 20:59:37 -0600

    Prueba contador descargas

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function myFunction() {
-  document.getElementById("negociosBooks").style.backgroundColor = "red";
-}
+    // Obtener el contador desde localStorage o iniciar en 0
+    let contador = parseInt(localStorage.getItem('contadorDescargas')) || 0;
+
+    // Mostrar el valor inicial
+    document.getElementById("numero-descargas").textContent = contador;
+
+    function incrementarContador() {
+      contador++;
+      document.getElementById("numero-descargas").textContent = contador;
+      localStorage.setItem('contadorDescargas', contador); // Guardar en localStorage
+    }
